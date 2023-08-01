f3d90a69488f3a7f2280bf9ac78cfbecaaba73e8
Sage <sbobgoss@gmail.com>
2023-08-01 16:28:23 -0400

    fix path issues in run convert mesh

--- run_convert_mesh.py
+++ run_convert_mesh.py
@@ -1,15 +1,15 @@
 import subprocess
 
 for i in range(5):
-    # command = f'python mesh_conversion/mesh_conversion.py -p regions/test_region_{i}.output.poly -n regions/test_region_{i}.node -e regions/test_region_{i}.ele'
+    # command = f'python mesh_conversion/mesh_conversion.py -p regions/test_example_{i}.output.poly -n regions/test_example_{i}.node -e regions/test_example_{i}.ele'
     subprocess.run([
         'python',
         'mesh_conversion/mesh_conversion.py',
         '-p',
-        f'regions/test_region_{i}.output.poly',
+        f'regions/test_example_{i}.output.poly',
         '-n',
-        f'regions/test_region_{i}.node',
+        f'regions/test_example_{i}.node',
         '-e',
-        f'regions/test_region_{i}.ele',
+        f'regions/test_example_{i}.ele',
     ])
     break
