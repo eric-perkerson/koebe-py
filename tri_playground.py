b0be39f265d6bef5b7a33b3a911b19b6707543a6
Eric Perkerson <e.l.perkerson@gmail.com>
2023-08-10 13:00:35 -0700

    Add changes for meshing 3_fold_sym in playground

--- tri_playground.py
+++ tri_playground.py
@@ -3,10 +3,11 @@ from pathlib import Path
 import matplotlib.pyplot as plt
 import matplotlib.image as img
 import numpy as np
+import subprocess
 
 from region import Region
 
-domain= Region.region_from_components(
+domain = Region.region_from_components(
     [
         [
             (2.0, 0.0),
@@ -15,7 +16,7 @@ domain= Region.region_from_components(
             (-2.0, 2.4492935982947064e-16),
             (-1.0000000000000009, -1.7320508075688767),
             (1.0, -1.7320508075688772)
-        ], 
+        ],
         [
             (0.9000000000000001, 2.4492935982947065e-17),
             (1.0, 0.17320508075688773),
@@ -40,21 +41,48 @@ domain= Region.region_from_components(
             (-0.4500000000000004, -1.12583302491977),
             (-0.6500000000000004, -1.12583302491977)
         ]
-    ]       
+    ]
 )
-with open("regions/3_fold_sym/3_fold_sym.poly",'w') as file:
+with open("regions/3_fold_sym/3_fold_sym.poly", 'w') as file:
     domain.write(file)
 
-
-file_stem = 'test_example_0'
+file_stem = '3_fold_sym'
 path = Path(f'regions/{file_stem}/{file_stem}')
 tri = Triangulation.read(path)
+
+
+subprocess.run([
+    'python',
+    'mesh_conversion/mesh_conversion.py',
+    '-p',
+    f'regions/3_fold_sym/3_fold_sym.output.poly',
+    '-n',
+    f'regions/3_fold_sym/3_fold_sym.node',
+    '-e',
+    f'regions/3_fold_sym/3_fold_sym.ele',
+])
+
+# Plot the PDE solution on the vertices (from Mathematica)
+pde_min = np.min(tri.pde_values)
+pde_max = np.max(tri.pde_values)
+pde_colors = (tri.pde_values - pde_min) / (pde_max - pde_min)
+
+fig = tri.region.figure('test')
+plt.scatter(
+    tri.vertices[:, 0],
+    tri.vertices[:, 1],
+    c=pde_colors
+)
+plt.show()
+
+# TODO: Figure out how to compute the singular vertex using the Triangulation object
+
 
 # Display the region
 tri.show('test.png', show_vertex_indices=False)
 plt.show()
 
-# present the region  
+# present the region
 # im = img.imread('test.png')
 # plt.imshow(im)
 # plt.show()
